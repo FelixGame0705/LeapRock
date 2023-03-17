dec97acfb28a785f01155bf910affcad392c19b0
Ason <ason.vohuynhson@gmail.com>
2023-03-17 17:05:57 +0700

    Base_Camera

---
+++ Assets/Scripts/GamePlay/Camera/CameraRuning.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Camera
+{
+    internal class CameraRuning : MonoBehaviour
+    {
+        [SerializeField] Transform _cameraTransform;
+        [SerializeField] float speed;
+
+        private void FixedUpdate()
+        {
+            _cameraTransform.transform.position += Vector3.forward * speed * Time.fixedDeltaTime;
+        }
+    }
+}
